eb4175d531818986374dfab94c5187a2386d940d
Herraj <herrajluhano@gmail.com>
2020-10-15 23:36:22 -0400

    Add add/remove Node buttons and findNode function

--- src/Tree.js
+++ src/Tree.js
@@ -3,14 +3,43 @@ import Tree from "react-d3-tree";
 import clone from "clone";
 
 const debugData = {
-    name: "1",
+    name: "Root",
     children: [
         {
-            name: "2"
+            name: "1",
+            children: [
+                {
+                    name: "12",
+                    children: [
+                        {
+                            name: "123",
+                            children: []
+                        }
+                    ]
+                }
+            ]
         },
         {
-            name: "2"
+            name: "2",
+            children: [{
+                name: "21",
+                children: []
+            },
+            {
+                name: "22",
+                children: [
+                    {
+                        name: "221",
+                        children: []
+                    },
+                    {
+                        name: "222",
+                        children: []
+                    }
+                ]
+            }]
         }
+
     ]
 };
 
@@ -24,30 +53,78 @@ export default class CenteredTree extends React.PureComponent {
         data: debugData
     };
 
-    injectedNodesCount = 0;
+    rootChildCount = 0;
 
-    addChildNode = () => {
-        const nextData = clone(this.state.data);
-        const target = nextData.children;
-        this.injectedNodesCount++;
+    findNode = (data, nodeName, nestingKey) => (
+        data.reduce((a, item) => {
+            if (a) return a;
+            if (item.name === nodeName) return item;
+            if (item[nestingKey]) return this.findNode(item[nestingKey], nodeName, nestingKey)
+        }, null)
+    );
+
+    addChildNodeToRoot = () => {
+        const newData = clone(this.state.data);
+        const target = newData.children;
+        this.rootChildCount++;
         target.push({
-            name: `Inserted Node ${this.injectedNodesCount}`,
-            id: `inserted-node-${this.injectedNodesCount}`
+            name: `Child ${this.rootChildCount}`,
+            id: `child-${this.rootChildCount}`,
+            children: [],
+            attributes: {
+                parent: 'Root'
+            }
         });
         this.setState({
-            data: nextData
+            data: newData
         });
     };
 
-    removeChildNode = () => {
-        const nextData = clone(this.state.data);
-        const target = nextData.children;
+    removeChildNodeFromRoot = () => {
+        const newData = clone(this.state.data);
+        const target = newData.children;
         target.pop();
-        this.injectedNodesCount--;
+        this.rootChildCount--;
         this.setState({
-            data: nextData
+            data: newData
         });
     };
+
+    addChildNode = (nodeData, evt) => {
+        //console.log(nodeData);
+        const nodeName = nodeData.name;
+
+        const newData = clone(this.state.data);
+        const target = newData.children.find((node) => { return node.name === nodeName });
+        console.log(newData.children)
+        console.log(nodeName)
+        const targetChildren = target.children;
+        const noOfTargetChildren = target.children.length;
+
+        targetChildren.push({
+            name: `${nodeName}-${noOfTargetChildren}`,
+            id: `child-${nodeName}-${noOfTargetChildren}`,
+            children: [],
+            attributes: {
+                parent: nodeName
+            }
+        });
+
+
+
+        //console.log("newData", newData)
+
+        this.setState({
+            data: newData
+        });
+
+        //console.log("state childern: ", this.state.data.children);
+    }
+
+    showEdgeDetails = (linkSource, linkTarget, evt) => {
+        console.log('node parent: ', linkSource)
+        console.log('node: ', linkTarget)
+    }
 
     componentDidMount() {
         // Get treeContainer's dimensions so we can center the tree
@@ -58,17 +135,26 @@ export default class CenteredTree extends React.PureComponent {
                 y: dimensions.height / 2
             }
         });
+
+        //console.log(this.findNode(this.state.data.children, '223', 'children'));
     }
 
     render() {
         return (
             <div style={containerStyles} ref={tc => (this.treeContainer = tc)}>
-                <button onClick={this.addChildNode}>Add Node</button>
-                <button onClick={this.removeChildNode}>Remove Node</button>
+                <button onClick={this.addChildNodeToRoot}>Add Node To Root</button>
+                <button onClick={this.removeChildNodeFromRoot}>Remove Node From Root</button>
+
+                {/* <button onClick={console.log(this.bob)}>Show state</button> */}
+
                 <Tree
                     data={this.state.data}
                     translate={this.state.translate}
                     orientation={"vertical"}
+                    pathFunc={'straight'}
+                    separation={{ siblings: 2 }}
+                    onClick={(nodeData, evt) => this.addChildNode(nodeData, evt)}
+                    onLinkClick={(linkSource, linkTarget, evt) => this.showEdgeDetails(linkSource, linkTarget, evt)}
                 />
             </div>
         );
